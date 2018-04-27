ea6bc6610123fed98f7403349ce4ab0d4256163d
Alexandros Melissas <psyam19@linode026.cs.nott.ac.uk>
2018-04-27 12:07:38 +0100

    first compemail commit

--- CompanyEmail.java
+++ CompanyEmail.java
@@ -4,6 +4,7 @@ public class CompanyEmail {
     private String subjectLine;
     private String emailMessage;
 
+    //1.1
     public CompanyEmail() {
         fromAddress = null;
         toAddress = null;
@@ -11,6 +12,7 @@ public class CompanyEmail {
         emailMessage = null;
     }
 
+    //1.2
     public CompanyEmail(String fAddress, String tAddress, String subLine, String eMessage) {
         fromAddress = fAddress;
         toAddress = tAddress;
@@ -18,18 +20,22 @@ public class CompanyEmail {
         emailMessage = eMessage;
     }
 
+    //1.3
     public String fromAddress() {
         return fromAddress;
     }
 
+    //1.4
     public String toAddress() {
         return toAddress;
     }
 
+    //1.5
     public String subjectLine() {
         return subjectLine;
     }
 
+    //1.6
     public String emailMessage() {
 //    recursive call to method
 //    bug fix by Raigo Jerva
@@ -37,26 +43,31 @@ public class CompanyEmail {
         return emailMessage;
     }
 
+    //1.7
     public void setFrom(String fromAddr) {
         if (fromAddr.contains("@")) {
             fromAddress = fromAddr;
         }
     }
 
+    //1.8
     public void setTo(String toAddr) {
         if (toAddr.contains("@")) {
             toAddress = toAddr;
         }
     }
 
+    //1.9
     public void setSubject(String subLine) {
         subjectLine = subLine;
     }
 
+    //1.10
     public void setMessage(String eMessage) {
         emailMessage = eMessage;
     }
 
+    //1.11
     public boolean isValid() {
         boolean isComplete = true;
         if (fromAddress == null) isComplete = false;
@@ -66,6 +77,7 @@ public class CompanyEmail {
         return isComplete;
     }
 
+    //1.12
     public String toString() {
         if (subjectLine.equals("")) {
             return "[no subject]";
---
+++ CompanyEmailTest.java
@@ -0,0 +1,72 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CompanyEmailTest {
+
+	@org.junit.Before
+	public void setup() {
+		CompanyEmail cem = new CompanyEmail();
+	}
+	
+	@org.junit.Test
+	public void testCompanyEmail() {
+		;
+	}
+
+	@org.junit.Test
+	public void testCompanyEmailStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testFromAddress() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testToAddress() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testSubjectLine() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testEmailMessage() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testSetFrom() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testSetTo() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testSetSubject() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testSetMessage() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testIsValid() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
