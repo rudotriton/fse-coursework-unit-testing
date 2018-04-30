0ba08b38d301f6bafc30423335f19519ed3b0db8
psyam19 <37447007+psyam19@users.noreply.github.com>
2018-04-30 18:21:56 +0100

    Meetup 2 - tests all pass

--- CompanyEmailTest.java
+++ CompanyEmailTest.java
@@ -1,51 +1,64 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+
 public class CompanyEmailTest {
 
 	private CompanyEmail cem;
-	public static final String EMAIL_VERIFICATION = "^([\\w-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$";
 	
 	@org.junit.Before
 	public void setup() {
 		CompanyEmail cem = new CompanyEmail();
-		//cem.fromAddress = "hey hoe";
 		}
 	
 	@org.junit.Test
 	public void testBasicConst() {
-		
+		cem = new CompanyEmail();
+		assertNull(cem.fromAddress());
+		assertNull(cem.toAddress());
+		assertNull(cem.emailMessage());
+		assertNull(cem.subjectLine());
 	}
 	
 	@org.junit.Test
-	public void testEmailAdresses() {
-		CompanyEmail goodConstructor = new CompanyEmail("haha@ha.com","lol@xd.com","hi mom","im dumb");
-			assertTrue(goodConstructor.fromAddress().matches(EMAIL_VERIFICATION));
-			assertTrue(goodConstructor.toAddress().matches(EMAIL_VERIFICATION));
-		CompanyEmail badConstructor = new CompanyEmail("invalid","notanemail","parrot","poop");
-			assertFalse(badConstructor.fromAddress().matches(EMAIL_VERIFICATION));
-			assertFalse(badConstructor.toAddress().matches(EMAIL_VERIFICATION));
-		CompanyEmail halfbadConstructor = new CompanyEmail("valid@email.com","notanemail","parrot","poop");
-			assertTrue(halfbadConstructor.fromAddress().matches(EMAIL_VERIFICATION));
-			assertFalse(halfbadConstructor.toAddress().matches(EMAIL_VERIFICATION));
-		CompanyEmail sndhalfbadConstructor = new CompanyEmail("invalid","valid@email.com","parrot","poop");
-			assertFalse(sndhalfbadConstructor.fromAddress().matches(EMAIL_VERIFICATION));
-			assertTrue(sndhalfbadConstructor.toAddress().matches(EMAIL_VERIFICATION));
+	public void testFullConst() {
+		cem = new CompanyEmail("haha@ha.com","lol@xd.com","hi mom","im dumb");
+		assertEquals("haha@ha.com",cem.fromAddress());
+		assertEquals("lol@xd.com",cem.toAddress());
+		assertEquals("hi mom",cem.subjectLine());
+		assertEquals("im dumb",cem.emailMessage());
+	}
+	
+//		CompanyEmail goodConstructor = new CompanyEmail("haha@ha.com","lol@xd.com","hi mom","im dumb");
+//			assertTrue(goodConstructor.fromAddress().matches(EMAIL_VERIFICATION));
+//			assertTrue(goodConstructor.toAddress().matches(EMAIL_VERIFICATION));
+//		CompanyEmail badConstructor = new CompanyEmail("invalid","notanemail","parrot","poop");
+//			assertFalse(badConstructor.fromAddress().matches(EMAIL_VERIFICATION));
+//			assertFalse(badConstructor.toAddress().matches(EMAIL_VERIFICATION));
+//		CompanyEmail halfbadConstructor = new CompanyEmail("valid@email.com","invalid","parrot","poop");
+//			assertTrue(halfbadConstructor.fromAddress().matches(EMAIL_VERIFICATION));
+//			assertFalse(halfbadConstructor.toAddress().matches(EMAIL_VERIFICATION));
+//		CompanyEmail sndhalfbadConstructor = new CompanyEmail("invalid","valid@email.com","parrot","poop");
+//			assertFalse(sndhalfbadConstructor.fromAddress().matches(EMAIL_VERIFICATION));
+//			assertTrue(sndhalfbadConstructor.toAddress().matches(EMAIL_VERIFICATION));
+
+	@org.junit.Test
+	public void testFromAddress() {
+		cem = new CompanyEmail();
+		assertNull(cem.fromAddress());
+		//cem = new CompanyEmail("haha@ha.com",null,null,null);
+		// NOTE: FIX THE FULL CONSTRUCTOR
+		cem.setFrom("haha@ha.com");
+		assertNotNull(cem.fromAddress());
 	}
 
-//	@org.junit.Test
-//	public void testFromAddress() {
-//		assertTrue("idodo@hdjddj.com".matches(EMAIL_VERIFICATION));
-//		assertFalse("idoddjddj.com".matches(EMAIL_VERIFICATION));
-//		assertFalse("idoddjddjom".matches(EMAIL_VERIFICATION));
-//	}
-
-//	@org.junit.Test
-//	public void testToAddress() {
-//		assertTrue("idodo@hdjddj.com".matches(EMAIL_VERIFICATION));
-//		assertFalse("idoddjddj.com".matches(EMAIL_VERIFICATION));
-//		assertFalse("idoddjddjom".matches(EMAIL_VERIFICATION));
-//	}
+	@org.junit.Test
+	public void testToAddress() {
+		cem = new CompanyEmail();
+		assertNull(cem.toAddress());
+		//cem = new CompanyEmail(null,"haha@ha.com",null,null);
+		cem.setTo("haha@ha.com");
+		assertEquals("haha@ha.com" , cem.toAddress());
+	}
 
 	@org.junit.Test
 	public void testSubjectLine() {
@@ -65,17 +78,28 @@ public class CompanyEmailTest {
 
 	@org.junit.Test
 	public void testSetFrom() {
-		fail("Not yet implemented");
+		cem  = new CompanyEmail();
+		assertNull(cem.fromAddress());
+		cem.setFrom("noatsignhere");
+		assertNull(cem.fromAddress());
+		cem.setFrom("atsign@at.com");
+		assertEquals("atsign@at.com",cem.fromAddress());
 	}
 
 	@org.junit.Test
 	public void testSetTo() {
-		fail("Not yet implemented");
+		cem  = new CompanyEmail();
+		assertNull(cem.toAddress());
+		cem.setTo("noatsignhere");
+		assertNull(cem.toAddress());
+		cem.setTo("atsign@at.com");
+		assertEquals("atsign@at.com",cem.toAddress());
 	}
 
 	@org.junit.Test
 	public void testSetSubject() {
 		cem = new CompanyEmail();
+		assertNull(cem.subjectLine());
 		cem.setSubject("this is a subject");
 		assertEquals("this is a subject", cem.subjectLine());	
 	}
@@ -83,13 +107,25 @@ public class CompanyEmailTest {
 	@org.junit.Test
 	public void testSetMessage() {
 		cem = new CompanyEmail();
+		assertNull(cem.emailMessage());
 		cem.setMessage("EMAIL MESSAGE HERE HAHA");
 		assertEquals("EMAIL MESSAGE HERE HAHA", cem.emailMessage());
 	}
 
 	@org.junit.Test
 	public void testIsValid() {
-		fail("Not yet implemented");
+		cem = new CompanyEmail();
+		assertFalse(cem.isValid());
+		cem = new CompanyEmail("banana@gmail.com","","","");
+		assertFalse(cem.isValid());
+		cem = new CompanyEmail("","banana@gmail.com","","");
+		assertFalse(cem.isValid());
+		cem = new CompanyEmail("","","hi mom","");
+		assertFalse(cem.isValid());
+		cem = new CompanyEmail("","","","hi dad");
+		assertFalse(cem.isValid());
+		cem = new CompanyEmail("banana@gmail.com","banana2@gmail.com","hi mom","hi dad");
+		assertTrue(cem.isValid());
 	}
 
 	@org.junit.Test
