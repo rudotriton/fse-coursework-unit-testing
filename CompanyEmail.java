32edc5838e0807835375c861f194c9af500e0c70
psyam19 <37447007+psyam19@users.noreply.github.com>
2018-04-30 18:23:00 +0100

    Meetup 2 - Tests all pass

--- CompanyEmail.java
+++ CompanyEmail.java
@@ -3,7 +3,10 @@ public class CompanyEmail {
     private String toAddress;
     private String subjectLine;
     private String emailMessage;
-
+    
+    public static final String EMAIL_VERIFICATION = "^([\\w-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$";
+	// REGEX added for checking email addresses for validity
+    
     //1.1
     public CompanyEmail() {
         fromAddress = null;
@@ -14,10 +17,24 @@ public class CompanyEmail {
 
     //1.2
     public CompanyEmail(String fAddress, String tAddress, String subLine, String eMessage) {
-        fromAddress = fAddress;
+        /*fromAddress = fAddress;
         toAddress = tAddress;
         subjectLine = subLine;
-        emailMessage = eMessage;
+        emailMessage = eMessage;*/
+    	
+    	// Addresses are not checked for validation (only getter functions called by constructor)
+    	// Spotted by Sam Kendrick and Alex Melissas
+    	
+//    	fromAddress = null;
+//        toAddress = null;
+//        subjectLine = null;
+//        emailMessage = null;
+    	
+    	setFrom(fAddress);
+    	setTo(tAddress);
+    	setSubject(subLine);
+    	setMessage(eMessage);
+    	
     }
 
     //1.3
@@ -45,16 +62,18 @@ public class CompanyEmail {
 
     //1.7
     public void setFrom(String fromAddr) {
-        if (fromAddr.contains("@")) {  // unsatisfactory email check
+        //if (fromAddr.contains("@")) {        // UNSATISFACTORY EMAIL CHECKING -- SAM / ALEX
+    	if (fromAddr.matches(EMAIL_VERIFICATION)) {
             fromAddress = fromAddr;
         }
     }
 
     //1.8
     public void setTo(String toAddr) {
-        if (toAddr.contains("@")) {
+        //if (toAddr.contains("@")) { 
+    	if (toAddr.matches(EMAIL_VERIFICATION)) {
             toAddress = toAddr;
-        }
+    	}
     }
 
     //1.9
@@ -70,10 +89,14 @@ public class CompanyEmail {
     //1.11
     public boolean isValid() {
         boolean isComplete = true;
-        if (fromAddress == null) isComplete = false;
-        if (toAddress == null) isComplete = false;
-        if (subjectLine == null) isComplete = false;
-        if (emailMessage == null) isComplete = false;
+//        if (fromAddress == null) isComplete = false;
+//        if (toAddress == null) isComplete = false;
+//        if (subjectLine == null) isComplete = false;
+//        if (emailMessage == null) isComplete = false;
+        	//        BUG -> not ANDing - if last thing true it's wrong.
+        	//		Spotted by Sam Kendrick & Alex Melissas
+        if( (fromAddress == null) || (toAddress == null) || (subjectLine == null) || (emailMessage == null))
+        		isComplete=false;
         return isComplete;
     }
 
