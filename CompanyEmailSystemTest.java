e30f7d401d7448ae89281d6f96d27795a62b0e02
Vasilis Ieropoulos <kirav96@gmail.com>
2018-04-30 17:02:39 +0100

    added my part

--- CompanyEmailSystemTest.java
+++ CompanyEmailSystemTest.java
@@ -133,6 +133,56 @@ public class CompanyEmailSystemTest {
 				" P = List [P]rojects, [num] = Open Project [num], A = [A]dd Project, X = " +
                 "E[x]it Software" + System.lineSeparator(),outContent.toString());
 		 }
+	
+//test by Vasilis Ieropoulos
+    @org.junit.Test
+    public void testmain2_addNewEmail() {
+        String input = "P"+"\n1"+"\nF"+"\nN" +"\nC"+"\nL" + "\nA" + "\nfrom@email.com" + "\nto@email.com" +
+                "\nsubjectline" + "\nmessage" + "\nX" + "\nX";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        CompanyEmailSystem.main(new String[]{});
+        assertEquals("What do you want to do?\n" + 
+                " P = List [P]rojects, [num] = Open Project [num], A = [A]dd Project, X = E[x]it" + System.lineSeparator() +
+                "1) Proj1 [Feasibility] - 4emails"+ System.lineSeparator() +
+                "2) Proj2 [Feasibility] - 3emails"+ System.lineSeparator() +
+                "3) Proj3 [Feasibility] - 3emails"+ System.lineSeparator() +
+                "What do you want to do?"+ System.lineSeparator() +
+                 " P = List [P]rojects, [num] = Open Project [num], A = [A]dd Project, X = E[x]it Software"+ System.lineSeparator()+
+                "What do you want to do?\n" + 
+                " L = [L]ist Emails, A = [A]dd Email, F = List Phase [F]olders, N = Move to [N]ext Phase, [num] = List Emails in Phase [num], C = List [C]ontacts, X =  E[x]it Project" + System.lineSeparator() +
+                "1) Feasibility - 4 Emails"+ System.lineSeparator() +
+               "What do you want to do?"+ System.lineSeparator() +
+                " L = [L]ist Emails, A = [A]dd Email, F = List Phase [F]olders, N = Move to [N]ext Phase, [num] = List Emails in Phase [num], C = List [C]ontacts, X =  E[x]it Project" + System.lineSeparator()+
+                "[Phase changed: Proj1 [Design]"+ System.lineSeparator() +
+                "What do you want to do?"+ "\n"+
+                " L = [L]ist Emails, A = [A]dd Email, F = List Phase [F]olders, N = Move to [N]ext Phase, [num] = List Emails in Phase [num], C = List [C]ontacts, X =  E[x]it Project" + System.lineSeparator()+
+               "1) me0@me.com"+ System.lineSeparator() +
+               "2) me3@me.com"+ System.lineSeparator() +
+               "3) me6@me.com"+ System.lineSeparator() +
+               "4) me9@me.com"+ System.lineSeparator() +
+               "What do you want to do?"+ "\n"+
+               " L = [L]ist Emails, A = [A]dd Email, F = List Phase [F]olders, N = Move to [N]ext Phase, [num] = List Emails in Phase [num], C = List [C]ontacts, X =  E[x]it Project" + System.lineSeparator()+
+               "Proj1 [Design]"+ System.lineSeparator() + "\n"+
+   				"   From                Subject"+ System.lineSeparator() +
+   				"--------------------------------"+ System.lineSeparator() +
+   				"What do you want to do?"+ "\n"+
+   				" L = [L]ist Emails, A = [A]dd Email, F = List Phase [F]olders, N = Move to [N]ext Phase, [num] = List Emails in Phase [num], C = List [C]ontacts, X =  E[x]it Project"+ System.lineSeparator()+
+                "Which email address is it from?" + System.lineSeparator() +
+                "Which email address is it to?" + System.lineSeparator() +
+                "What is the Subject?" + System.lineSeparator() +
+                "What is the Message?" + System.lineSeparator() +
+                "[Email added to Proj1 [Design]]" + System.lineSeparator() +
+                "What do you want to do?\n" +
+                " L = [L]ist Emails, A = [A]dd Email, F = List Phase [F]olders, N = " +
+                "Move to [N]ext Phase, [num] = List Emails in Phase [num], C = List " +
+                "[C]ontacts, X =  E[x]it Project" + System.lineSeparator() + "What do " +
+                        "you want to do?\n" + " P =" +
+                " List [P]rojects, [num] = Open Project [num], A = [A]dd Project, X = " +
+                "E[x]it Software" + System.lineSeparator() +  "Goodbye!" + System.lineSeparator(), outContent
+                .toString());
+
+    }
 
     @org.junit.After
     public void cleanUpStreams() {
