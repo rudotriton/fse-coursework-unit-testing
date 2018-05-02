dd2f7ec3d9d2126a12b5e21dde4ad78ff1ce2cca
psyam19 <37447007+psyam19@users.noreply.github.com>
2018-05-02 13:07:01 +0100

    clean!

--- CompanyEmailTest.java
+++ CompanyEmailTest.java
@@ -1,144 +1,141 @@
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class CompanyEmailTest {
 
-    private CompanyEmail cem;
-
-    @org.junit.Before
-    public void setup() {
-        cem = new CompanyEmail();
-    }
-
-    //    Test by Sam Kendrick & Alex Melissas
-    @org.junit.Test
-    public void testBasicConst() {
-        assertNull(cem.fromAddress());
-        assertNull(cem.toAddress());
-        assertNull(cem.emailMessage());
-        assertNull(cem.subjectLine());
-    }
-
-    //    Test by Sam Kendrick & Alex Melissas
-    @org.junit.Test
-    public void testFullConst() {
-        cem = new CompanyEmail("haha@ha.com", "lol@xd.com", "hi mom", "im dumb");
-        assertEquals("haha@ha.com", cem.fromAddress());
-        assertEquals("lol@xd.com", cem.toAddress());
-        assertEquals("hi mom", cem.subjectLine());
-        assertEquals("im dumb", cem.emailMessage());
-    }
-
-//		CompanyEmail goodConstructor = new CompanyEmail("haha@ha.com","lol@xd.com","hi mom","im dumb");
-//			assertTrue(goodConstructor.fromAddress().matches(EMAIL_VERIFICATION));
-//			assertTrue(goodConstructor.toAddress().matches(EMAIL_VERIFICATION));
-//		CompanyEmail badConstructor = new CompanyEmail("invalid","notanemail","parrot","poop");
-//			assertFalse(badConstructor.fromAddress().matches(EMAIL_VERIFICATION));
-//			assertFalse(badConstructor.toAddress().matches(EMAIL_VERIFICATION));
-//		CompanyEmail halfbadConstructor = new CompanyEmail("valid@email.com","invalid","parrot","poop");
-//			assertTrue(halfbadConstructor.fromAddress().matches(EMAIL_VERIFICATION));
-//			assertFalse(halfbadConstructor.toAddress().matches(EMAIL_VERIFICATION));
-//		CompanyEmail sndhalfbadConstructor = new CompanyEmail("invalid","valid@email.com","parrot","poop");
-//			assertFalse(sndhalfbadConstructor.fromAddress().matches(EMAIL_VERIFICATION));
-//			assertTrue(sndhalfbadConstructor.toAddress().matches(EMAIL_VERIFICATION));
-
-    //    Test by Sam Kendrick & Alex Melissas
-    @org.junit.Test
-    public void testFromAddress() {
-        assertNull(cem.fromAddress());
-        //cem = new CompanyEmail("haha@ha.com",null,null,null);
-        // NOTE: FIX THE FULL CONSTRUCTOR
-        cem.setFrom("haha@ha.com");
-        assertNotNull(cem.fromAddress());
-    }
-
-    //    Test by Sam Kendrick & Alex Melissas
-    @org.junit.Test
-    public void testToAddress() {
-        assertNull(cem.toAddress());
-        //cem = new CompanyEmail(null,"haha@ha.com",null,null);
-        cem.setTo("haha@ha.com");
-        assertEquals("haha@ha.com", cem.toAddress());
-    }
-
-    //    Test by Sam Kendrick & Alex Melissas
-    @org.junit.Test
-    public void testSubjectLine() {
-        assertNull(cem.subjectLine());
-        cem = new CompanyEmail("haha@ha.com", "lol@xd.com", "hi mom", "hi dad");
-        assertEquals("hi mom", cem.subjectLine());
-    }
-
-    //    Test by Sam Kendrick & Alex Melissas
-    @org.junit.Test
-    public void testEmailMessage() {
-        assertNull(cem.emailMessage());
-        cem = new CompanyEmail("haha@ha.com", "lol@xd.com", "hi mom", "hi dad");
-        assertEquals("hi dad", cem.emailMessage());
-    }
-
-    //    Test by Sam Kendrick & Alex Melissas
-    @org.junit.Test
-    public void testSetFrom() {
-        assertNull(cem.fromAddress());
-        cem.setFrom("noatsignhere");
-        assertNull(cem.fromAddress());
-        cem.setFrom("atsign@at.com");
-        assertEquals("atsign@at.com", cem.fromAddress());
-    }
-
-    //    Test by Sam Kendrick & Alex Melissas
-    @org.junit.Test
-    public void testSetTo() {
-        assertNull(cem.toAddress());
-        cem.setTo("noatsignhere");
-        assertNull(cem.toAddress());
-        cem.setTo("atsign@at.com");
-        assertEquals("atsign@at.com", cem.toAddress());
-    }
-
-    //    Test by Sam Kendrick & Alex Melissas
-    @org.junit.Test
-    public void testSetSubject() {
-        assertNull(cem.subjectLine());
-        cem.setSubject("this is a subject");
-        assertEquals("this is a subject", cem.subjectLine());
-    }
-
-    //    Test by Sam Kendrick & Alex Melissas
-    @org.junit.Test
-    public void testSetMessage() {
-        assertNull(cem.emailMessage());
-        cem.setMessage("EMAIL MESSAGE HERE HAHA");
-        assertEquals("EMAIL MESSAGE HERE HAHA", cem.emailMessage());
-    }
-
-    //    Test by Sam Kendrick & Alex Melissas
-    @org.junit.Test
-    public void testIsValid() {
-        assertFalse(cem.isValid());
-        cem = new CompanyEmail("banana@gmail.com", "", "", "");
-        assertFalse(cem.isValid());
-        cem = new CompanyEmail("", "banana@gmail.com", "", "");
-        assertFalse(cem.isValid());
-        cem = new CompanyEmail("", "", "hi mom", "");
-        assertFalse(cem.isValid());
-        cem = new CompanyEmail("", "", "", "hi dad");
-        assertFalse(cem.isValid());
-        cem = new CompanyEmail("banana@gmail.com", "banana2@gmail.com", "hi mom", "hi dad");
-        assertTrue(cem.isValid());
-    }
-
-    //    Test by Sam Kendrick & Alex Melissas
-    @org.junit.Test
-    public void testToString() {
-        //System.out.println(cem.toString());
-        //assertNull(cem.toString());		test should fail when subjectLine is null, but assertNull not working on output
-        cem.setSubject("");
-        assertEquals("[no subject]", cem.toString());
-        //assertNotEquals("a subject", cem.toString());
-        cem.setSubject("a new subject");
-        assertEquals("a new subject", cem.toString());
-
-    }
-
+    // CLARITY -> Added numbering above each test according to the ID's 
+    // we had for the functions on our test and bugfixing report from the previous lab assignment.
+    // By Alex Melissas & Sam Kendrick	
+	
+	// ISSUE 1 -> When setting values in constructor to null, it doesn't work properly. It works as if
+	// they were set to null if we set them to the empty string "".
+	
+	// ISSUE 2 -> Using set functions when testing get functions
+	
+	private CompanyEmail cem;
+	
+	@org.junit.Before	// Needed to create an empty CompanyEmail object to use in our tests.
+	public void setup() {
+		cem = new CompanyEmail();
+		}
+	
+	//Test 1.1
+	@org.junit.Test
+	public void testBasicConst() {
+		assertNull(cem.fromAddress());
+		assertNull(cem.toAddress());
+		assertNull(cem.emailMessage());
+		assertNull(cem.subjectLine());
+	}
+	
+	//Test 1.2
+	@org.junit.Test
+	public void testFullConst() {
+		cem = new CompanyEmail("haha@ha.com","lol@xd.com","hi mom","hi dad");
+		assertEquals("haha@ha.com",cem.fromAddress());
+		assertEquals("lol@xd.com",cem.toAddress());
+		assertEquals("hi mom",cem.subjectLine());
+		assertEquals("hi dad",cem.emailMessage());
+	}
+	
+	//Test 1.3
+	@org.junit.Test
+	public void testFromAddress() {
+		assertNull(cem.fromAddress());
+		cem = new CompanyEmail("haha@ha.com","","","");
+		//cem.setFrom("haha@ha.com");
+		assertNotNull(cem.fromAddress());
+	}
+	
+	//Test 1.4
+	@org.junit.Test
+	public void testToAddress() {
+		assertNull(cem.toAddress());
+		cem = new CompanyEmail("","haha@ha.com","","");
+		//cem.setTo("haha@ha.com");
+		assertEquals("haha@ha.com" , cem.toAddress());
+	}
+	
+	//Test 1.5
+	@org.junit.Test
+	public void testSubjectLine() {
+		assertNull(cem.subjectLine());
+		cem = new CompanyEmail("","","hi mom","");
+		assertEquals("hi mom", cem.subjectLine());
+	}
+	
+	//Test 1.6
+	@org.junit.Test
+	public void testEmailMessage() {
+		assertNull(cem.emailMessage());
+		cem = new CompanyEmail("","","","hi dad");
+		assertEquals("hi dad", cem.emailMessage());
+	}
+	
+	//Test 1.7
+	@org.junit.Test
+	public void testSetFrom() {
+		assertNull(cem.fromAddress());
+		cem.setFrom("notanemail");
+		assertNull(cem.fromAddress());
+		cem.setFrom("stillnotanemail@"); // this would fail because of improper email validation in class
+		assertNull(cem.fromAddress());
+		cem.setFrom("validemail@site.com");
+		assertEquals("validemail@site.com",cem.fromAddress());
+	}
+	
+	//Test 1.8
+	@org.junit.Test
+	public void testSetTo() {
+		assertNull(cem.toAddress());
+		cem.setTo("notanemail");
+		assertNull(cem.toAddress());
+		cem.setFrom("stillnotanemail@"); // this would fail because of improper email validation in class
+		assertNull(cem.fromAddress());
+		cem.setTo("validemail@site.com");
+		assertEquals("validemail@site.com",cem.toAddress());
+	}
+	
+	//Test 1.9
+	@org.junit.Test
+	public void testSetSubject() {
+		assertNull(cem.subjectLine());
+		cem.setSubject("this is a subject");
+		assertEquals("this is a subject", cem.subjectLine());	
+	}
+	
+	//Test 1.10
+	@org.junit.Test
+	public void testSetMessage() {
+		assertNull(cem.emailMessage());
+		cem.setMessage("EMAIL MESSAGE HERE HAHA");
+		assertEquals("EMAIL MESSAGE HERE HAHA", cem.emailMessage());
+	}
+	
+	//Test 1.11
+	@org.junit.Test
+	public void testIsValid() {
+		assertFalse(cem.isValid());
+		cem = new CompanyEmail("banana@gmail.com","","","");
+		assertFalse(cem.isValid());
+		cem = new CompanyEmail("","banana@gmail.com","","");
+		assertFalse(cem.isValid());
+		cem = new CompanyEmail("","","hi mom","");
+		assertFalse(cem.isValid());
+		cem = new CompanyEmail("","","","hi dad");
+		assertFalse(cem.isValid());
+		cem = new CompanyEmail("banana@gmail.com","banana2@gmail.com","hi mom","hi dad");
+		assertTrue(cem.isValid());
+	}
+	
+	//Test 1.12
+	@org.junit.Test
+	public void testToString() {
+		assertEquals(cem.subjectLine(),cem.toString());
+		cem.setSubject("");
+		assertEquals("[no subject]",cem.toString());
+		assertNotEquals("a subject", cem.toString());
+		cem.setSubject("a new subject");
+		assertEquals("a new subject",cem.toString());
+	}
 }
